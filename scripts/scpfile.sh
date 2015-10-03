88841da2b58816658219d83e23d2bb7eac313024
sayanh <sayan.mnit@gmail.com>
2015-10-03 17:09:47 +0200

    Changed the scripts for remote logging and added performance logs.

--- scripts/scpfile.sh
+++ scripts/scpfile.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 FILE=$1
+DEST=$2
+
 echo "------------ Transferring "$FILE " to VM1"
+scp $1 anarchy@vm1:$DEST
+
+echo "------------ Transferring "$FILE " to VM2"
+scp $1 anarchy@vm2:$DEST
+
+
---
+++ src/java/de/tum/viewmaintenance/EvaluationLogging/StatsLogging.java
@@ -0,0 +1,7 @@
+package de.tum.viewmaintenance.EvaluationLogging;
+
+/**
+ * Created by shazra on 10/2/15.
+ */
+public class StatsLogging {
+}
